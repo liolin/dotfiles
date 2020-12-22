f5dc994463808b4655e5f434ff158354d157fb07
Olivier Lischer <olivier.lischer@liolin.ch>
2020-12-22 11:03:19 +0100

    Run new scripts on login

--- xmonad/.xmonad/autostart.sh
+++ xmonad/.xmonad/autostart.sh
@@ -6,5 +6,8 @@ picom --config ~/.config/picom.conf &
 nitrogen --restore &
 setxkbmap -layout ch,us -option grp:alt_space_toggle &
 nm-applet &
+playerctld daemon
+xmodmap ~/.config/xmodmap/xmodmap.conf
+/bin/sh ~/bin/update-notify
 
 bash ~/.config/polybar/launch.sh &
