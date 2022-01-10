98a7304d2bf04c89f9926e82ed3b0924747a93ce
Olivier Lischer <olivier.lischer@liolin.ch>
2022-01-10 10:53:59 +0100

    Improve tangle.el script

--- tangle.el
+++ tangle.el
@@ -1,6 +1,12 @@
-(defun liomacs/tangle()
-  (with-current-buffer (find-file-noselect "/tmp/git-test/someorg.org")
+#!/usr/bin/emacs --script
+
+(defun liomacs/tangle(file)
+  (with-current-buffer (find-file-noselect file)
     (org-babel-tangle)))
 
+(defun main ()
+  (setq org-confirm-babel-evaluate nil)
+  (liomacs/tangle (car command-line-args-left)))
 
-(liomacs/tangle)
+
+(main)
